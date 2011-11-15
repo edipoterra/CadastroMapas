3aac265e548113fa648adb0481ad634a8ed6fcfd
edipoterra <edipoterra@gmail.com>
2011-11-15 11:49:04 -0200

    Initial Commit

---
+++ src/com/example/AdressMapsActivity/AdressMapsActivity.java
@@ -0,0 +1,15 @@
+package com.example.AdressMapsActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AdressMapsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
